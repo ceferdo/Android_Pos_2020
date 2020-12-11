91ce59adaa8f96e90b9305da1614d452dc3fb317
Celso Fernando Paes <ceferdo@gmail.com>
2020-12-10 22:22:27 -0300

    aula 20201210

--- app/src/main/java/com/aula/pos/MainActivity.kt
+++ app/src/main/java/com/aula/pos/MainActivity.kt
@@ -1,38 +1,32 @@
 package com.aula.pos
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-    }
+        calculo.setOnClickListener{
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
+            val gasolina = (valorGasolina.text.toString()).toDouble()
+            val alcool = (valorAlcool.text.toString()).toDouble()
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+            val calculo = gasolina * 0.7
+
+            if (calculo > alcool) {
+                resultado.text = "Alcool é melhor"
+            } else {
+                resultado.text = "Gasolina é melhor"
+            }
+
+            Toast.makeText(this@MainActivity, "Apertei o login", Toast.LENGTH_LONG).show();
+            Log.i("Mobile" , "Cliquei no botão");
         }
     }
 }
